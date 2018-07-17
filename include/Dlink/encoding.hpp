d9288ca36fe6807ab4f81342bd1a79b0db78788c
static <kmc7468@naver.com>
2018-07-17 16:47:54 +0900

    encoding::utf16be 정적 클래스 추가
    이 클래스는 UTF-16(빅 엔디안)과 UTF-32 사이의 문자열 인코딩을 지원합니다.

--- include/Dlink/encoding.hpp
+++ include/Dlink/encoding.hpp
@@ -91,6 +91,49 @@ namespace dlink::encoding
 		static constexpr std::int32_t max_encoded_length = 2;
 		static constexpr std::uint8_t byte_order_mark[] = { 0xFF, 0xFE };
 	};
+
+	class utf16be final
+	{
+	public:
+		utf16be() = delete;
+		utf16be(const utf16be& encoding) = delete;
+		utf16be(utf16be&& encoding) noexcept = delete;
+		~utf16be() = default;
+
+	public:
+		utf16be& operator=(const utf16be& encoding) = delete;
+		utf16be& operator=(utf16be&& encoding) noexcept = delete;
+		bool operator==(const utf16be& encoding) = delete;
+		bool operator!=(const utf16be& encoding) = delete;
+
+	public:
+		static std::u16string encode_char(char32_t character);
+		static std::u16string encode(const std::u32string_view& string);
+		static char32_t decode_char(const std::u16string_view& string);
+		static char32_t decode_char(char16_t first, char16_t second = 0);
+		static std::u32string decode(const std::u16string_view& string);
+		static std::size_t encoded_length(char32_t character);
+		static std::size_t encoded_length(const std::u32string_view& string);
+		static std::size_t decoded_length(char16_t first);
+		static std::size_t decoded_length(const std::u16string_view& string);
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+		static std::wstring wencode_char(char32_t character);
+		static std::wstring wencode(const std::u32string_view& string);
+		static char32_t wdecode_char(const std::wstring_view& string);
+		static char32_t wdecode_char(wchar_t first, wchar_t second = 0);
+		static std::u32string wdecode(const std::wstring_view& string);
+		static std::size_t wdecoded_length(wchar_t first);
+		static std::size_t wdecoded_length(const std::wstring_view& string);
+#endif
+
+	public:
+		static constexpr const char* name = "UTF-16BE";
+		static constexpr std::uint16_t codepage = 1201;
+		static constexpr bool is_little_endian = false;
+		static constexpr std::int32_t max_encoded_length = 2;
+		static constexpr std::uint8_t byte_order_mark[] = { 0xFE, 0xFF };
+	};
 }
 
 #endif
--- src/encoding.cpp
+++ src/encoding.cpp
@@ -519,3 +519,280 @@ namespace dlink::encoding
 	}
 #endif
 }
+
+namespace dlink::encoding
+{
+	std::u16string utf16be::encode_char(char32_t character)
+	{
+		if (is_invalid_unicode(character))
+			throw std::invalid_argument("The argument 'character' isn't a valid Unicode character.");
+
+		std::size_t length = encoded_length(character);
+
+		if (length == 1)
+		{
+			if (get_endian() == endian::little_endian)
+			{
+				std::swap(*(reinterpret_cast<std::uint8_t*>(&character) + 2),
+						  *(reinterpret_cast<std::uint8_t*>(&character) + 3));
+			}
+
+			return { static_cast<char16_t>(character) };
+		}
+		else
+		{
+			char32_t temp = character - 0x10000;
+
+			char16_t high_surrogate = static_cast<char16_t>((temp / 0x400) + 0xD800);
+			char16_t low_surrogate = static_cast<char16_t>((temp % 0x400) + 0xDC00);
+
+			if (get_endian() == endian::little_endian)
+			{
+				std::swap(*reinterpret_cast<std::uint8_t*>(&high_surrogate),
+						  *(reinterpret_cast<std::uint8_t*>(&high_surrogate) + 1));
+				std::swap(*reinterpret_cast<std::uint8_t*>(&low_surrogate),
+						  *(reinterpret_cast<std::uint8_t*>(&low_surrogate) + 1));
+			}
+
+			return { high_surrogate, low_surrogate };
+		}
+	}
+	std::u16string utf16be::encode(const std::u32string_view& string)
+	{
+		if (string.empty())
+			return u"";
+
+		std::u16string result;
+
+		for (char32_t character : string)
+		{
+			result.append(encode_char(character));
+		}
+
+		return result;
+	}
+	char32_t utf16be::decode_char(const std::u16string_view& string)
+	{
+		if (string.empty())
+			return 0;
+		if (string.size() > max_encoded_length)
+			throw std::invalid_argument("The argument 'string' isn't a valid Unicode character encoded in UTF-16BE.");
+
+		char16_t first = string[0];
+		char16_t second;
+
+		if (get_endian() == endian::little_endian)
+		{
+			std::swap(*reinterpret_cast<std::uint8_t*>(&first),
+					  *(reinterpret_cast<std::uint8_t*>(&first) + 1));
+		}
+
+		if (encoded_length(first) == 1)
+		{
+			return first;
+		}
+		else
+		{
+			if (string.size() == 1)
+				throw std::invalid_argument("The argument 'string' isn't a valid Unicode character encoded in UTF-16BE.");
+
+			second = string[1];
+
+			if (get_endian() == endian::little_endian)
+			{
+				std::swap(*reinterpret_cast<std::uint8_t*>(&second),
+						  *(reinterpret_cast<std::uint8_t*>(&second) + 1));
+			}
+
+			return (first << 16) + second;
+		}
+	}
+	char32_t utf16be::decode_char(char16_t first, char16_t second)
+	{
+		if (get_endian() == endian::little_endian)
+		{
+			std::swap(*reinterpret_cast<std::uint8_t*>(&first),
+					  *(reinterpret_cast<std::uint8_t*>(&first) + 1));
+		}
+
+		if (encoded_length(first) == 1)
+		{
+			return first;
+		}
+		else
+		{
+			if (get_endian() == endian::little_endian)
+			{
+				std::swap(*reinterpret_cast<std::uint8_t*>(&second),
+						  *(reinterpret_cast<std::uint8_t*>(&second) + 1));
+			}
+
+			return (first << 16) + second;
+		}
+	}
+	std::u32string utf16be::decode(const std::u16string_view& string)
+	{
+		if (string.empty())
+			return U"";
+
+		using ssize_t = std::make_signed_t<std::size_t>;
+
+		std::u32string result;
+
+		for (std::size_t i = 0; i < string.size(); )
+		{
+			char16_t character = string[i];
+			std::size_t length = encoded_length(character);
+
+			switch (length)
+			{
+			case 1:
+			{
+				result.push_back(decode_char(character));
+				i += 1;
+
+				break;
+			}
+
+			case 2:
+			{
+				if (static_cast<ssize_t>(string.size()) - static_cast<ssize_t>(i) - 2 < 0)
+					throw std::invalid_argument("The argument 'string' isn't a valid string encoded in UTF-16BE.");
+
+				result.push_back(decode_char(character, string[i + 1]));
+				i += 2;
+
+				break;
+			}
+			}
+		}
+
+		return result;
+	}
+	std::size_t utf16be::encoded_length(char32_t character)
+	{
+		if (is_invalid_unicode(character))
+			throw std::invalid_argument("The argument 'character' isn't a valid Unicode character.");
+
+		if (character < 0x10000)
+		{
+			return 1;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+	std::size_t utf16be::encoded_length(const std::u32string_view& string)
+	{
+		if (string.empty())
+			return 0;
+
+		std::size_t result = 0;
+
+		for (char32_t character : string)
+		{
+			result += encoded_length(character);
+		}
+
+		return result;
+	}
+	std::size_t utf16be::decoded_length(char16_t first)
+	{
+		if (first >= 0xD800 && first <= 0xDBFF)
+		{
+			return 2;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	std::size_t utf16be::decoded_length(const std::u16string_view& string)
+	{
+		if (string.empty())
+			return 0;
+
+		std::size_t result = 0;
+
+		for (std::size_t i = 0; i < string.size(); )
+		{
+			char16_t character = string[i];
+			std::size_t length = decoded_length(character);
+
+			result += length;
+			i += length;
+		}
+
+		return result;
+	}
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+	std::wstring utf16be::wencode_char(char32_t character)
+	{
+		std::wstring result;
+		std::u16string temp = encode_char(character);
+
+		result.insert(result.end(), temp.begin(), temp.end());
+		return result;
+	}
+	std::wstring utf16be::wencode(const std::u32string_view& string)
+	{
+		std::wstring result;
+		std::u16string temp = encode(string);
+
+		result.insert(result.end(), temp.begin(), temp.end());
+		return result;
+	}
+	char32_t utf16be::wdecode_char(const std::wstring_view& string)
+	{
+		switch (string.length())
+		{
+		case 1:
+			return decode_char(static_cast<char16_t>(string[0]));
+
+		case 2:
+			return decode_char(static_cast<char16_t>(string[0]), static_cast<char16_t>(string[1]));
+
+		default:
+			throw std::invalid_argument("The argument 'string' isn't a valid Unicode character encoded in UTF-16BE.");
+		}
+	}
+	char32_t utf16be::wdecode_char(wchar_t first, wchar_t second)
+	{
+		return decode_char(static_cast<char16_t>(first), static_cast<char16_t>(second));
+	}
+	std::u32string utf16be::wdecode(const std::wstring_view& string)
+	{
+		if (string.empty())
+			return U"";
+
+		std::u32string result;
+
+		for (wchar_t character : string)
+		{
+			result.push_back(wdecode_char(character));
+		}
+
+		return result;
+	}
+	std::size_t utf16be::wdecoded_length(wchar_t first)
+	{
+		return decoded_length(static_cast<char16_t>(first));
+	}
+	std::size_t utf16be::wdecoded_length(const std::wstring_view& string)
+	{
+		if (string.empty())
+			return 0;
+
+		std::size_t result = 0;
+
+		for (wchar_t character : string)
+		{
+			result += wdecoded_length(character);
+		}
+
+		return result;
+	}
+#endif
+}
