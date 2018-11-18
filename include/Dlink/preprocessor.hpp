7cd93687c71fc43eecbe7edb64cdb50293210aaf
static <kmc7468@naver.com>
2018-11-18 15:59:36 +0900

    macro 클래스 추가
    이 클래스는 전처리기 매크로에 대한 정보를 저장하는 클래스입니다.

--- include/Dlink/preprocessor.hpp
+++ include/Dlink/preprocessor.hpp
@@ -4,6 +4,8 @@
 #include <Dlink/compiler_metadata.hpp>
 #include <Dlink/source.hpp>
 
+#include <cstddef>
+#include <string_view>
 #include <vector>
 
 namespace dlink
@@ -26,6 +28,76 @@ namespace dlink
 		static bool preprocess(compiler_metadata& metadata, std::vector<source>& sources);
 		static bool preprocess_singlethread(compiler_metadata& metadata, std::vector<source>& sources);
 		static bool preprocess_source(source& source, compiler_metadata& metadata);
+	};
+
+	class macro_base
+	{
+	public:
+		virtual ~macro_base() = default;
+
+	protected:
+		macro_base(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept;
+		macro_base(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+				   const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept;
+		macro_base(const macro_base& macro) noexcept;
+
+	public:
+		bool operator==(const macro_base& macro) const = delete;
+		bool operator!=(const macro_base& macro) const = delete;
+
+	protected:
+		macro_base& operator=(const macro_base& macro) noexcept;
+
+	public:
+		virtual bool need_arguments() const noexcept = 0;
+		virtual std::string replaced(const std::string_view& arguments) const = 0;
+
+	public:
+		std::string_view name() const noexcept;
+		void name(const std::string_view& new_name) noexcept;
+		std::string_view scope_begin_line_data() const noexcept;
+		void scope_begin_line_data(const std::string_view& new_scope_begin_line_data) noexcept;
+		std::string_view scope_end_line_data() const noexcept;
+		void scope_end_line_data(const std::string_view& new_scope_end_line_data) noexcept;
+		std::size_t scope_begin_line() const noexcept;
+		void scope_begin_line(std::size_t new_scope_begin_line) noexcept;
+		std::size_t scope_end_line() const noexcept;
+		void scope_end_line(std::size_t new_scope_end_line) noexcept;
+
+	private:
+		std::string_view name_;
+		std::string_view scope_begin_line_data_;
+		std::string_view scope_end_line_data_;
+		std::size_t scope_begin_line_;
+		std::size_t scope_end_line_ = 0;
+	};
+
+	class macro final : public macro_base
+	{
+	public:
+		macro(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept;
+		macro(const std::string_view& name, const std::string_view& replaced, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept;
+		macro(const std::string_view& name,
+			  const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+			  const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept;
+		macro(const std::string_view& name, const std::string_view& replaced,
+			  const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+			  const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept;
+		macro(const macro& macro) noexcept;
+		virtual ~macro() override = default;
+
+	public:
+		macro& operator=(const macro& macro) noexcept;
+		bool operator==(const macro& macro) const = delete;
+		bool operator!=(const macro& macro) const = delete;
+
+	public:
+		virtual bool need_arguments() const noexcept override;
+		virtual std::string replaced(const std::string_view&) const override;
+		void replaced(const std::string_view& new_replaced) noexcept;
+		
+	private:
+		std::string_view replaced_;
 	};
 }
 
--- src/preprocessor.cpp
+++ src/preprocessor.cpp
@@ -54,7 +54,7 @@ namespace dlink
 	{
 		if (source.state() < source_state::decoded)
 			throw invalid_state("The state of the argument 'source' must be 'dlink::source_state::decoded' or higher when 'static bool dlink::preprocessor::preprocess_source(dlink::source&, dlink::compiler_metadata&)' method is called.");
-	
+		
 		using memstream = boost::iostreams::stream<boost::iostreams::basic_array_source<char>>;
 
 		std::vector<std::string> result;
@@ -85,6 +85,8 @@ namespace dlink
 			while (is_whitespace(line_stream, next_c));
 			if (line_stream.eof()) continue_with_append;
 			if (next_c != '#') continue_with_append;
+
+
 
 			const std::size_t offset = static_cast<std::size_t>(line_stream.tellg());
 			if (offset >= length)
@@ -190,5 +192,117 @@ namespace dlink
 		}
 
 		return ok;
+	}
+}
+
+namespace dlink
+{
+	macro_base::macro_base(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept
+		: name_(name), scope_begin_line_data_(scope_begin_line_data), scope_begin_line_(scope_begin_line)
+	{}
+	macro_base::macro_base(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+						   const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept
+		: name_(name), scope_begin_line_data_(scope_begin_line_data), scope_begin_line_(scope_begin_line),
+		scope_end_line_data_(scope_end_line_data), scope_end_line_(scope_end_line)
+	{}
+	macro_base::macro_base(const macro_base& macro) noexcept
+		: name_(macro.name_), scope_begin_line_data_(macro.scope_begin_line_data_), scope_begin_line_(macro.scope_begin_line_),
+		scope_end_line_data_(macro.scope_end_line_data_), scope_end_line_(macro.scope_end_line_)
+	{}
+
+	macro_base& macro_base::operator=(const macro_base& macro) noexcept
+	{
+		name_ = macro.name_;
+		scope_begin_line_data_ = macro.scope_begin_line_data_;
+		scope_begin_line_ = macro.scope_begin_line_;
+		scope_end_line_data_ = macro.scope_end_line_data_;
+		scope_end_line_ = macro.scope_end_line_;
+
+		return *this;
+	}
+
+	std::string_view macro_base::name() const noexcept
+	{
+		return name_;
+	}
+	void macro_base::name(const std::string_view& new_name) noexcept
+	{
+		name_ = new_name;
+	}
+	std::string_view macro_base::scope_begin_line_data() const noexcept
+	{
+		return scope_begin_line_data_;
+	}
+	void macro_base::scope_begin_line_data(const std::string_view& new_scope_begin_line_data) noexcept
+	{
+		scope_begin_line_data_ = new_scope_begin_line_data;
+	}
+	std::string_view macro_base::scope_end_line_data() const noexcept
+	{
+		return scope_end_line_data_;
+	}
+	void macro_base::scope_end_line_data(const std::string_view& new_scope_end_line_data) noexcept
+	{
+		scope_end_line_data_ = new_scope_end_line_data;
+	}
+	std::size_t macro_base::scope_begin_line() const noexcept
+	{
+		return scope_begin_line_;
+	}
+	void macro_base::scope_begin_line(std::size_t new_scope_begin_line) noexcept
+	{
+		scope_begin_line_ = new_scope_begin_line;
+	}
+	std::size_t macro_base::scope_end_line() const noexcept
+	{
+		return scope_end_line_;
+	}
+	void macro_base::scope_end_line(std::size_t new_scope_end_line) noexcept
+	{
+		scope_end_line_ = new_scope_end_line;
+	}
+}
+
+namespace dlink
+{
+	macro::macro(const std::string_view& name, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept
+		: macro_base(name, scope_begin_line_data, scope_begin_line)
+	{}
+	macro::macro(const std::string_view& name, const std::string_view& replaced, const std::string_view& scope_begin_line_data, std::size_t scope_begin_line) noexcept
+		: macro_base(name, scope_begin_line_data, scope_begin_line), replaced_(replaced)
+	{}
+	macro::macro(const std::string_view& name,
+				 const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+				 const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept
+		: macro_base(name, scope_begin_line_data, scope_begin_line, scope_end_line_data, scope_end_line)
+	{}
+	macro::macro(const std::string_view& name, const std::string_view& replaced,
+				 const std::string_view& scope_begin_line_data, std::size_t scope_begin_line,
+				 const std::string_view& scope_end_line_data, std::size_t scope_end_line) noexcept
+		: macro_base(name, scope_begin_line_data, scope_begin_line, scope_end_line_data, scope_end_line), replaced_(replaced)
+	{}
+	macro::macro(const macro& macro) noexcept
+		: macro_base(macro), replaced_(macro.replaced_)
+	{}
+
+	macro& macro::operator=(const macro& macro) noexcept
+	{
+		macro_base::operator=(macro);
+		replaced_ = macro.replaced_;
+
+		return *this;
+	}
+
+	bool macro::need_arguments() const noexcept
+	{
+		return false;
+	}
+	std::string macro::replaced(const std::string_view&) const
+	{
+		return std::string(replaced_);
+	}
+	void macro::replaced(const std::string_view& new_replaced) noexcept
+	{
+		replaced_ = new_replaced;
 	}
 }
